ba96cbabadd4c64ad9f873c8411438898f893ba1
Caminare <gxvitor.1997@hotmail.com>
2019-06-20 22:15:25 -0300

    Add files via upload

---
+++ front-desktop-react/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
+
+const Header = () => (
+    <header id="main-header">
+        <a href="/">JSHunt</a> 
+    </header>
+);
+
+export default Header;
